416bff087c7a5682e6aceb7756688b7b6737ecc2
Pratham Sharma <165146063+PrathamSharma98@users.noreply.github.com>
2026-01-04 00:50:49 +0530

    Remove BrowserRouter and use Routes directly

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import CityDetail from "./pages/CityDetail";
 import WardDetail from "./pages/WardDetail";
@@ -18,17 +19,17 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/city/:cityId" element={<CityDetail />} />
-          <Route path="/ward/:wardId" element={<WardDetail />} />
-          <Route path="/map" element={<MapView />} />
-          <Route path="/alerts" element={<Alerts />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/city/:cityId" element={<CityDetail />} />
+        <Route path="/ward/:wardId" element={<WardDetail />} />
+        <Route path="/map" element={<MapView />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
